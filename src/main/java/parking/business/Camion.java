330b1dc857a5abae0aa17215452def52f04d2497
Julian-Arnaud <julian.arnaud.z10@gmail.com>
2015-01-14 09:42:26 +0100

    Ajout des sous vehicules

---
+++ src/main/java/parking/business/Camion.java
@@ -0,0 +1,13 @@
+package parking.business;
+
+public class Camion extends Vehicule{
+
+	public Camion(String imm, String mod, String mar, String prop) {
+		super(imm, mod, mar, prop);
+	}
+
+	public boolean isTransporteur() {
+		return true;
+	}
+
+}
---
+++ src/main/java/parking/business/Moto.java
@@ -0,0 +1,12 @@
+package parking.business;
+
+public class Moto extends Vehicule{
+
+	public Moto(String imm, String mod, String mar, String prop) {
+		super(imm, mod, mar, prop);
+	}
+
+	public boolean isTransporteur() {
+		return false;
+	}
+}
