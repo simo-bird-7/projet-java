bd4307846b2169bbaa9293c160daddf542cea661
rob1fo <alonzo.robin7@gmail.com>
2015-01-14 08:52:35 +0100

    Modifications de la classe Place

--- src/main/java/parking/business/Place.java
+++ src/main/java/parking/business/Place.java
@@ -1,9 +1,19 @@
 package parking.business;
 
+import parking.exception.PlusAucunePlaceException;
+
 public abstract class Place {
 	
+	int numero;
+	static int nbInstance;
 	Vehicule vehicule = null; // le vehicule garé à cette place
+	boolean reserve = false; 
 
+	public Place(){
+		numero = nbInstance;
+		nbInstance++;
+	}
+	
 	public boolean isFree()
 	{
 		return (vehicule == null);
@@ -12,15 +22,23 @@ public abstract class Place {
 	public abstract void park(Vehicule v);
 	
 	public void libere() {
-		// TODO Auto-generated method stub
-		
+		vehicule = null;
 	}
+	
 	public Vehicule getParkedVehicule() {
-		// TODO Auto-generated method stub
-		return null;
+		return vehicule;
 	}
-	public boolean reserve() {
-		// TODO Auto-generated method stub
-		return false;
+	
+	public boolean reserve(){
+		return reserve;
+	}
+	
+	public void reserver() throws PlusAucunePlaceException {
+		if (reserve == true){
+			throw(new PlusAucunePlaceException());
+		}
+		else{
+			reserve = true;
+		}
 	}
 }
